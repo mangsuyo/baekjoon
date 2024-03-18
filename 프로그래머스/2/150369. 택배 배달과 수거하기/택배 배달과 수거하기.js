85a213b9a39a512d129721ed2e4c2f7f4d46a047
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2024-03-18 20:07:41 +0900

    [level 2] Title: 택배 배달과 수거하기, Time: 5.05 ms, Memory: 39.2 MB -BaekjoonHub

--- 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
+++ 프로그래머스/2/150369. 택배 배달과 수거하기/택배 배달과 수거하기.js
@@ -1,12 +1,15 @@
 function solution(cap, n, deliveries, pickups) {
-  
     let answer = 0;
     let have_to_deli = 0;
     let have_to_pick = 0;
-    
     for(let i = n - 1; i >= 0; i--){
-        have_to_deli -= cap;
-        
+        have_to_deli += deliveries[i];
+        have_to_pick += pickups[i];
+        while(have_to_deli > 0 || have_to_pick > 0){
+            have_to_deli -= cap;
+            have_to_pick -= cap;
+            answer += (i + 1) * 2;
+        }
     }
-
+    return answer;
 }
