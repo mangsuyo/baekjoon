1082727389c182e32a1b30e1f5505ac6b269892a
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2024-03-21 14:02:18 +0900

    [Silver I] Title: 카드 합체 놀이, Time: 256 ms, Memory: 13648 KB -BaekjoonHub

---
+++ 백준/Silver/15903. 카드 합체 놀이/카드 합체 놀이.js
@@ -0,0 +1,88 @@
+class MinHeap {
+  constructor() {
+    this.heap = [null];
+  }
+
+  size() {
+    return this.heap.length - 1;
+  }
+
+  getMin() {
+    return this.heap[1] ? this.heap[1] : null;
+  }
+
+  swap(a, b) {
+    [this.heap[a], this.heap[b]] = [this.heap[b], this.heap[a]];
+  }
+
+  heappush(value) {
+    this.heap.push(value);
+    let curIdx = this.heap.length - 1;
+    let parIdx = (curIdx / 2) >> 0;
+
+    while (curIdx > 1 && this.heap[parIdx] > this.heap[curIdx]) {
+      this.swap(parIdx, curIdx);
+      curIdx = parIdx;
+      parIdx = (curIdx / 2) >> 0;
+    }
+  }
+
+  heappop() {
+    const min = this.heap[1];
+    if (this.heap.length <= 2) this.heap = [null];
+    else this.heap[1] = this.heap.pop();
+
+    let curIdx = 1;
+    let leftIdx = curIdx * 2;
+    let rightIdx = curIdx * 2 + 1;
+
+    if (!this.heap[leftIdx]) return min;
+    if (!this.heap[rightIdx]) {
+      if (this.heap[leftIdx] < this.heap[curIdx]) {
+        this.swap(leftIdx, curIdx);
+      }
+      return min;
+    }
+
+    while (
+      this.heap[leftIdx] < this.heap[curIdx] ||
+      this.heap[rightIdx] < this.heap[curIdx]
+    ) {
+      const minIdx =
+        this.heap[leftIdx] > this.heap[rightIdx] ? rightIdx : leftIdx;
+      this.swap(minIdx, curIdx);
+      curIdx = minIdx;
+      leftIdx = curIdx * 2;
+      rightIdx = curIdx * 2 + 1;
+    }
+
+    return min;
+  }
+}
+
+function solution(input) {
+  const [N, M] = input[0].split(" ").map(Number);
+  const cards = input[1].split(" ").map(Number);
+  const heap = new MinHeap();
+  for (let card of cards) {
+    heap.heappush(card);
+  }
+
+  for (let i = 0; i < M; i++) {
+    const minSum = heap.heappop() + heap.heappop();
+    heap.heappush(minSum);
+    heap.heappush(minSum);
+  }
+
+  let answer = 0n;
+  for (let i = heap.size(); i >= 1; i--) {
+    answer += BigInt(heap.heap[i]);
+  }
+
+  console.log(answer.toString());
+}
+
+const fs = require("fs");
+const file = process.platform === "linux" ? "/dev/stdin" : "input.txt";
+const input = fs.readFileSync(file).toString().trim().split("\n");
+solution(input);
