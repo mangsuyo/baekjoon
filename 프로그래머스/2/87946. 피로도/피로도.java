71a1eada474f9d02599540fcde30355a03b77016
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2024-07-09 17:08:21 +0900

    [level 2] Title: 피로도, Time: 0.06 ms, Memory: 94.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,41 +1,32 @@
-import static java.util.Comparator.*;
-
 import java.util.*;
 
-public class Solution {
+class Solution {
+
+	int answer = 0;
 
 	public int solution(int k, int[][] dungeons) {
-		Arrays.sort(dungeons, Comparator.<int[]>comparingInt((a) -> a[0]).reversed()
-			.thenComparingInt(a -> a[1]));
-
-		List<List<Integer>> list = new ArrayList<>();
-
-		List<Integer> numbers = new ArrayList<>();
-
-		for (int i = 0; i < dungeons.length; i++) {
-			if (dungeons[i][0] <= k) {
-				list.add(new ArrayList<>(Arrays.asList(dungeons[i][0], dungeons[i][1])));
-				backtracking(i, k, dungeons, list, numbers, 0);
-				break;
-			}
-		}
-		return Collections.max(numbers);
+		boolean[] visited = new boolean[dungeons.length];
+		backtracking(k, dungeons, visited, 0);
+		return answer;
 	}
 
-	public void backtracking(int start, int k, int[][] dungeons, List<List<Integer>> list, List<Integer> numbers,
-		int count) {
+	public void backtracking(int k, int[][] dungeons, boolean[] visited, int count) {
 
-		if (k < list.get(start).get(0)) {
-			numbers.add(count);
-			return;
+		if(count >= answer){
+			answer = count;
 		}
 
-		for (int i = start; i < dungeons.length - 1; i++) {
-			list.add(Arrays.asList(dungeons[i][0], dungeons[i][1]));
-			backtracking(i + 1, k, dungeons, list, numbers, count + 1);
-			list.remove(list.size() - 1);
+		for(int i = 0; i < dungeons.length; i++){
+			if(!visited[i]) {
+				int[] dungeonsInfo = dungeons[i];
+				int needFatigue = dungeonsInfo[0];
+				int useFatigue = dungeonsInfo[1];
+				if (k >= needFatigue) {
+					visited[i] = true;
+					backtracking(k - useFatigue, dungeons, visited, count + 1);
+					visited[i] = false;
+				}
+			}
 		}
-
-		numbers.add(count);
 	}
 }
