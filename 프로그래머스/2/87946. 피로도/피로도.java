8239fac44f85dd59e6a300ab2db27084bb758f27
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2024-08-08 09:39:18 +0900

    [level 2] Title: 피로도, Time: 2.69 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,29 +1,28 @@
 import java.util.*;
 
 class Solution {
-
 	int answer = 0;
 
 	public int solution(int k, int[][] dungeons) {
-		boolean[] visited = new boolean[dungeons.length];
-		backtracking(k, dungeons, visited, 0);
+		int n = dungeons.length;
+		boolean[] visited = new boolean[n];
+		backtrack(k, dungeons, visited, 0);
 		return answer;
 	}
 
-	public void backtracking(int k, int[][] dungeons, boolean[] visited, int count) {
+	private void backtrack(int k, int[][] dungeons, boolean[] visited, int count) {
 
 		if(count >= answer){
 			answer = count;
 		}
-
+		
 		for(int i = 0; i < dungeons.length; i++){
-			if(!visited[i]) {
-				int[] dungeonsInfo = dungeons[i];
-				int needFatigue = dungeonsInfo[0];
-				int useFatigue = dungeonsInfo[1];
-				if (k >= needFatigue) {
+			if(k >= dungeons[i][0]){
+				if(!visited[i]) {
+					k -= dungeons[i][1];
 					visited[i] = true;
-					backtracking(k - useFatigue, dungeons, visited, count + 1);
+					backtrack(k, dungeons, visited, count + 1);
+					k += dungeons[i][1];
 					visited[i] = false;
 				}
 			}
