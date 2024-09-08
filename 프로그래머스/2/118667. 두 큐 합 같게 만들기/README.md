53d7bed19908cbaa8671db838ccfeeeb98a05527
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2024-09-08 17:46:28 +0900

    [level 2] Title: 두 큐 합 같게 만들기, Time: 52.02 ms, Memory: 136 MB -BaekjoonHub

--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 두 큐 합 같게 만들기 - 118667 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667#qna) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/118667) 
 
 ### 성능 요약
 
-메모리: 157 MB, 시간: 40.56 ms
+메모리: 136 MB, 시간: 52.02 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 07월 13일 00:19:37
+2024년 09월 08일 17:46:22
 
 ### 문제 설명
 
--- 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
+++ 프로그래머스/2/118667. 두 큐 합 같게 만들기/두 큐 합 같게 만들기.java
@@ -1,47 +1,69 @@
 import java.util.*;
 
 class Solution {
-
 	public int solution(int[] queue1, int[] queue2) {
+		int answer = 0;
+		int[] sums = getTotal(queue1, queue2);
+		long sumA = sums[0];
+		long sumB = sums[1];
+
+		int lenA = queue1.length;
+		int lenB = queue2.length;
+
+		int loop = 0;
+
+		if ((sumA + sumB) % 2 != 0)
+			return -1;
+
 		Queue<Integer> queueA = new ArrayDeque<>();
 		Queue<Integer> queueB = new ArrayDeque<>();
 
-		long limit = queue1.length * 3;
-		int answer = -1;
-		long sumA = 0;
-		long sumB = 0;
-
-		for (int num : queue1) {
-			queueA.add(num);
-			sumA += num;
+		for (int i = 0; i < queue1.length; i++) {
+			queueA.add(queue1[i]);
 		}
 
-		for (int num : queue2) {
-			queueB.add(num);
-			sumB += num;
-		}
-
-		if((sumA + sumB) % 2 != 0){
-			return -1;
+		for (int i = 0; i < queue2.length; i++) {
+			queueB.add(queue2[i]);
 		}
 
 		while (!queueA.isEmpty() && !queueB.isEmpty()) {
-			if(answer > limit) return -1;
-			answer += 1;
-			if (sumA < sumB) {
-				int num = queueB.poll();
-				queueA.add(num);
-				sumB -= num;
-				sumA += num;
-			} else if (sumA > sumB) {
+			if(loop > (lenA + lenB) * 3){
+				break;
+			}
+			if (sumA == sumB)
+				return answer;
+			else if (sumA > sumB) {
 				int num = queueA.poll();
 				queueB.add(num);
 				sumA -= num;
 				sumB += num;
+				answer += 1;
 			} else {
-				return answer;
+				int num = queueB.poll();
+				queueA.add(num);
+				sumA += num;
+				sumB -= num;
+				answer += 1;
 			}
+			loop += 1;
 		}
+
 		return -1;
+
+	}
+
+	public int[] getTotal(int[] queue1, int[] queue2) {
+		int sumA = 0;
+		int sumB = 0;
+
+		for (int i = 0; i < queue1.length; i++) {
+			sumA += queue1[i];
+		}
+
+		for (int i = 0; i < queue2.length; i++) {
+			sumB += queue2[i];
+		}
+
+		return new int[] {sumA, sumB, sumA + sumB};
 	}
 }
