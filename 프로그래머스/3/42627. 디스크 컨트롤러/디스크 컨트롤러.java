68f25f94b65be404c13c9903db568c68b2c5f89f
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-08-16 18:11:49 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.71 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,30 +1,37 @@
 import java.util.*;
+
 class Solution {
     public int solution(int[][] jobs) {
         int answer = 0;
-        int curTime = 0;
         int count = 0;
-        int jobIndex = 0;
-        int n = jobs.length;
-        Queue<int[]> queue = new PriorityQueue<>((a, b) -> a[1] - b[1]);
-        Arrays.sort(jobs, (a, b) -> a[0] - b[0]);
+        Arrays.sort(jobs, (a, b) -> {
+            if(a[0] != b[0]) return a[0] - b[0];
+            return a[1] - b[1];
+        });
+        Queue<int[]> queue = new PriorityQueue<>((t1, t2) -> t1[1] - t2[1]);
         
-        while(count < n){
-            while(jobIndex < n && jobs[jobIndex][0] <= curTime){
+        int curTime = 0;
+        int jobIndex = 0;
+        int total = 0;
+        while(count < jobs.length){            
+            while(jobIndex < jobs.length && jobs[jobIndex][0] <= curTime){
                 queue.offer(new int[]{jobs[jobIndex][0], jobs[jobIndex][1]});
                 jobIndex += 1;
             }
             
-            if(!queue.isEmpty()){
-                int[] task = queue.poll();
-                curTime += task[1];
-                answer += curTime - task[0];
-                count += 1;
+            if(queue.isEmpty()){
+                if(jobIndex < jobs.length){
+                    curTime = jobs[jobIndex][0];
+                }
             }
             else{
-                curTime = jobs[jobIndex][0];
+                int[] task = queue.poll();
+                curTime += task[1];
+                total += curTime - task[0];
+                count += 1;
             }
         }
-        return answer / n;
+        
+        return total / count;
     }
 }
