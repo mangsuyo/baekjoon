b07b63bba66bacf53e5bb40a712781b8975eb7d9
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-08-26 17:58:29 +0900

    [level 1] Title: 신규 아이디 추천, Time: 0.42 ms, Memory: 82.8 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,81 +1,72 @@
-import java.util.*;
-
 class Solution {
     public String solution(String new_id) {
-        String answer = "";
         
         // 1단계
-        String stepOne = new_id.toLowerCase();
+        String one = new_id.toLowerCase();
         
         // 2단계
-        StringBuilder temp = new StringBuilder();
-        for(int i = 0; i < stepOne.length(); i++){
-            char c = stepOne.charAt(i);
+        StringBuilder sb = new StringBuilder();
+        for(int i = 0; i < one.length(); i++){
+            char c = one.charAt(i);
             if('a' <= c && c <= 'z'){
-                temp.append(c);
+                sb.append(c);
             }
             else if('0' <= c && c <= '9'){
-                temp.append(c);
+                sb.append(c);
             }
             else if(c == '-' || c == '_' || c == '.'){
-                temp.append(c);
+                sb.append(c);
             }
         }
         
-        String stepTwo = temp.toString();
-        temp = new StringBuilder();
-        boolean flag = false;
         // 3단계
-        for(int i = 0; i < stepTwo.length(); i++){
-            char c = stepTwo.charAt(i);
+        String two = sb.toString();
+        sb = new StringBuilder();
+        
+        int flag = 0;
+        for(int i = 0; i < two.length(); i++){
+            char c = two.charAt(i);
             if(c == '.'){
-                if(!flag){
-                    temp.append(c);
-                    flag = true;
-                }
-            }else{
-                flag = false;
-                temp.append(c);
+                if(flag == 0){
+                    sb.append(c);
+                    flag = 1;
+                }                    
+            }
+            else{
+                flag = 0;
+                sb.append(c);
             }
         }
         
-        String stepThree = temp.toString();
-                
         // 4단계
-        if(temp.length() > 0){
-            if(temp.charAt(0) == '.'){
-                temp.deleteCharAt(0);
-            }
+        if(sb.length() > 0 && sb.charAt(0) == '.'){
+            sb.deleteCharAt(0);
+        }
+        if(sb.length() > 0 && sb.charAt(sb.length() - 1) == '.'){
+            sb.deleteCharAt(sb.length() - 1);
         }
         
-        
-            if(temp.length() > 0 && temp.charAt(temp.length() - 1) == '.'){
-                temp.deleteCharAt(temp.length() - 1);
-            }
         // 5단계
-        
-        if(temp.length() == 0){
-            temp.append("a");
+        if(sb.length() == 0){
+            sb.append("a");
         }
         
         // 6단계
-        String stepFive = temp.toString();
-        
-        if(stepFive.length() >= 16){
-            temp = new StringBuilder(stepFive.substring(0, 15));
-            if(temp.charAt(temp.length() - 1) == '.'){
-                temp.deleteCharAt(temp.length() - 1);
+        if(sb.length() >= 16){
+            sb = new StringBuilder(sb.substring(0, 15));
+            if(sb.charAt(sb.length() - 1) == '.'){
+                sb.deleteCharAt(sb.length() - 1);
             }
         }
         
         // 7단계
-        if(temp.length() <= 2){
-            while(temp.length() != 3){
-                temp.append(temp.charAt(temp.length() -1));
+        if(sb.length() <= 2){
+            while(sb.length() != 3){
+                sb.append(sb.charAt(sb.length() - 1));
             }
         }
         
-            
-        return temp.toString();
+        
+        return sb.toString();
     }
 }
