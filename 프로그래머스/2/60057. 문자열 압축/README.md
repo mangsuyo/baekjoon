b3bb88b07a00daaf5a2b19c711c7ee13b92c7dc8
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-08-26 17:44:57 +0900

    [level 2] Title: 문자열 압축, Time: 4.49 ms, Memory: 93 MB -BaekjoonHub

--- 프로그래머스/2/60057. 문자열 압축/README.md
+++ 프로그래머스/2/60057. 문자열 압축/README.md
@@ -1,10 +1,10 @@
 # [level 2] 문자열 압축 - 60057 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/60057) 
 
 ### 성능 요약
 
-메모리: 85.7 MB, 시간: 4.97 ms
+메모리: 93 MB, 시간: 4.49 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 08월 25일 20:56:41
+2025년 08월 26일 17:44:55
 
 ### 문제 설명
 
--- 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
+++ 프로그래머스/2/60057. 문자열 압축/문자열 압축.java
@@ -1,32 +1,33 @@
-import java.util.*;
-
 class Solution {
     
     int n;
     
-    public int solution(String s) {
+    public int solution(String s) { 
         n = s.length();
         int answer = n;
         
         for(int i = 1; i <= n; i++){
-            answer = Math.min(answer, getPressedSize(i, s));
+            answer = Math.min(answer, getPressedStr(i, s));
         }
-        
         return answer;
     }
     
-    int getPressedSize(int step, String s){
+    int getPressedStr(int step, String s){
         StringBuilder sb = new StringBuilder();
         String prev = s.substring(0, step);
+        
         int count = 1;
+        
         for(int i = step; i < n; i += step){
             String current;
-            if(i + step > n){
-                current = s.substring(i);
+            
+            if(i + step <= n){
+                current = s.substring(i, i + step);               
             }
             else{
-                current = s.substring(i, i + step);
+                current = s.substring(i);
             }
+            
             if(prev.equals(current)){
                 count += 1;
             }
@@ -35,14 +36,17 @@ class Solution {
                     sb.append(count);
                 }
                 sb.append(prev);
-                prev = current;
                 count = 1;
+                prev = current;
             }
         }
-            if(count > 1){
-                sb.append(count);
-            }
-            sb.append(prev);
+        
+        if(count > 1){
+            sb.append(count);
+        }
+        sb.append(prev);
+        
         return sb.length();
+        
     }
 }
