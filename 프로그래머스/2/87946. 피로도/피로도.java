a5ef0327872bca3035aec139d310f5ea4c7a6cd9
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-10-13 13:45:49 +0900

    [level 2] Title: 피로도, Time: 5.11 ms, Memory: 83 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,31 +1,36 @@
 import java.util.*;
 
 class Solution {
-	int answer = 0;
-
-	public int solution(int k, int[][] dungeons) {
-		int n = dungeons.length;
-		boolean[] visited = new boolean[n];
-		backtrack(k, dungeons, visited, 0);
-		return answer;
-	}
-
-	private void backtrack(int k, int[][] dungeons, boolean[] visited, int count) {
-
-		if(count >= answer){
-			answer = count;
-		}
-		
-		for(int i = 0; i < dungeons.length; i++){
-			if(k >= dungeons[i][0]){
-				if(!visited[i]) {
-					k -= dungeons[i][1];
-					visited[i] = true;
-					backtrack(k, dungeons, visited, count + 1);
-					k += dungeons[i][1];
-					visited[i] = false;
-				}
-			}
-		}
-	}
+    
+    int n;
+    int m;
+    
+    int answer = 0;
+    
+    boolean[] visited;
+    
+    public int solution(int k, int[][] dungeons) {
+        this.n = dungeons.length;
+        this.m = dungeons[0].length;
+        
+        visited = new boolean[n];
+        backtrack(dungeons, k, 0);
+        return answer;
+    }
+    
+    void backtrack(int[][] dungeons, int k, int count){
+        if(answer < count) answer = count;
+        
+        for(int i = 0; i < n; i++){
+            int[] dungeon = dungeons[i];
+            int required = dungeon[0];
+            int cost = dungeon[1];
+            if(k >= required && !visited[i]){
+                visited[i] = true;
+                backtrack(dungeons, k - cost, count + 1);
+                visited[i] = false;
+            }
+            
+        }
+    }
 }
