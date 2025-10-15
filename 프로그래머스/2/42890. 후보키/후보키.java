4790500b59ea772e1eb0fae6907511c2da7bf607
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-10-15 11:02:29 +0900

    [level 2] Title: 후보키, Time: 5.85 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/42890. 후보키/후보키.java
+++ 프로그래머스/2/42890. 후보키/후보키.java
@@ -1,69 +1,55 @@
 import java.util.*;
 
 class Solution {
-
-	int n;
-	int m;
-
-	List<List<Integer>> uniqueKeys = new ArrayList<>();
-
-	public int solution(String[][] relation) {
-		n = relation.length;
-		m = relation[0].length;
-		int answer = 0;
-
-		List<List<Integer>> candidateKeys = new ArrayList<>();
-
-		backtrack(candidateKeys, new ArrayList<>(), 0);
-		
-		Collections.sort(candidateKeys, Comparator.comparingInt(item -> item.size()));
-		
-		for(int i = 0; i < candidateKeys.size(); i++){
-			List<Integer> cols = candidateKeys.get(i);
-			isCandidateKey(relation, cols);
-		}
-
-		return uniqueKeys.size();
-	}
-
-	void backtrack(List<List<Integer>> candidateKeys, List<Integer> list, int start) {
-
-		if(!list.isEmpty()){
-			candidateKeys.add(new ArrayList<>(list));
-		}
-
-		for(int i = start; i < m; i++){
-			if(!list.contains(i)){
-				list.add(i);
-				backtrack(candidateKeys, list, i + 1);
-				list.remove(list.size() - 1);
-			}
-		}
-	}
-
-	boolean isCandidateKey(String[][] relation, List<Integer> cols) {
-
-		for(int i = 0 ; i < uniqueKeys.size(); i++){
-			if(cols.containsAll(uniqueKeys.get(i))){
-				return false;
-			}
-		}
-
-		Set<List<String>> sets = new HashSet<>();
-		for (int i = 0; i < n; i++) {
-			List<String> list = new ArrayList<>();
-			for (int j = 0; j < cols.size(); j++) {
-				int col = cols.get(j);
-				list.add(relation[i][col]);
-			}
-			sets.add(list);
-		}
-
-		if(sets.size() == n){
-			uniqueKeys.add(new ArrayList<>(cols));
-			return true;
-		}
-
-		return false;
-	}
+    
+    int n;
+    int m;
+    Set<List<Integer>> answers;
+    
+    public int solution(String[][] relation) {
+        int answer = 0;
+        n = relation.length;
+        m = relation[0].length;
+        
+        List<List<Integer>> candidates = new ArrayList<>();
+        makeCandidates(0, candidates, new ArrayList<>());
+        
+        candidates.sort((a, b) -> a.size() - b.size());
+        
+        answers = new HashSet<>();
+        for(List<Integer> candidate: candidates){
+            if(isCandidateKey(relation, candidate)){
+                answer += 1;
+                answers.add(candidate);
+            }
+        }
+        return answer;
+    }
+    
+    boolean isCandidateKey(String[][] relation, List<Integer> candidate){
+        for(List<Integer> answer: answers){
+            if(candidate.containsAll(answer)) return false;
+        }
+        Set<List<String>> set = new HashSet<>();
+        for(int i = 0; i < n; i++){
+            List<String> temp = new ArrayList<>();
+            for(Integer key: candidate){
+                temp.add(relation[i][key]);
+            }
+            set.add(temp);
+        }
+        return set.size() == n;
+    }
+    
+    void makeCandidates(int start, List<List<Integer>> candidates, List<Integer> list){
+        if(list.size() > 0){
+            candidates.add(new ArrayList<>(list));
+        }
+        
+        for(int i = start; i < m; i++){
+            list.add(i);
+            makeCandidates(i + 1, candidates, list);
+            list.remove(list.size() - 1);
+        }
+    }
 }
