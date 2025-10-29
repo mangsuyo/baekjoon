884352ae2605ab7f82a19c4bcf1e2267a7057c65
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-10-29 20:25:23 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.31 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -1,35 +1,54 @@
 import java.util.*;
 
 class Solution {
+    
+    // 언제끝났는지만 알면됨.
+    class Job{
+        int index;
+        int requestTime;
+        int costTime;
+        
+        public Job(int index, int requestTime, int costTime){
+            this.index = index;
+            this.requestTime = requestTime;
+            this.costTime = costTime;
+        }
+    }
+    
+    
     public int solution(int[][] jobs) {
-        int answer = 0;
-        Arrays.sort(jobs, (j1, j2) -> j1[0] - j2[0]);
-        Queue<int[]> queue = new PriorityQueue<>((j1, j2) -> j1[1] - j2[1]);
-        
-        int currentTime = 0;
-        int count = 0;
-        int n = jobs.length;
-        int jobIndex = 0;
-        
-        while(count < n){
-            while(jobIndex < n && jobs[jobIndex][0] <= currentTime){
-                queue.offer(new int[]{jobs[jobIndex][0], jobs[jobIndex][1]});
-                jobIndex++;
-            }
-
-            if(!queue.isEmpty()){
-                int[] job = queue.poll();
-                currentTime += job[1];
-                answer += currentTime - job[0];
-                count += 1;
-            }
-            
-            else{
-                currentTime = jobs[jobIndex][0];
-            }
+        List<Job> list = new ArrayList<>();
+        for(int i = 0; i < jobs.length; i++){
+            list.add(new Job(i, jobs[i][0], jobs[i][1]));
         }
         
+        Queue<Job> queue = new PriorityQueue<>((p1, p2) -> {
+            if(p1.costTime == p2.costTime){
+                if(p1.requestTime == p2.requestTime){
+                    return p1.index - p2.index;
+                }
+                else{
+                    return p1.requestTime - p2.requestTime;
+                }
+            }
+            else{
+                return p1.costTime - p2.costTime;
+            }
+        });
         
-        return answer / n;
+            
+        for(Job j: list){
+            queue.offer(j);
+        }
+        
+        int answer = 0;
+        int time = 0;
+        while(!queue.isEmpty()){
+            Job cur = queue.poll();
+            if(time < cur.requestTime) time = cur.requestTime;
+            time = time + cur.costTime;
+            answer += time - cur.requestTime;
+        }
+        return answer / jobs.length;
     }
 }
