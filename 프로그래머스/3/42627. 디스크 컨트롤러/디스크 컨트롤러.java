7169a8a00426e592c79128bebb39c3805dc267b5
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-10-29 20:24:58 +0900

    [level 3] Title: 디스크 컨트롤러, Time: 3.31 ms, Memory: 76.9 MB -BaekjoonHub

--- 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
+++ 프로그래머스/3/42627. 디스크 컨트롤러/디스크 컨트롤러.java
@@ -3,35 +3,33 @@ import java.util.*;
 class Solution {
     public int solution(int[][] jobs) {
         int answer = 0;
-        int count = 0;
-        Arrays.sort(jobs, (a, b) -> {
-            if(a[0] != b[0]) return a[0] - b[0];
-            return a[1] - b[1];
-        });
-        Queue<int[]> queue = new PriorityQueue<>((t1, t2) -> t1[1] - t2[1]);
+        Arrays.sort(jobs, (j1, j2) -> j1[0] - j2[0]);
+        Queue<int[]> queue = new PriorityQueue<>((j1, j2) -> j1[1] - j2[1]);
         
-        int curTime = 0;
+        int currentTime = 0;
+        int count = 0;
+        int n = jobs.length;
         int jobIndex = 0;
-        int total = 0;
-        while(count < jobs.length){            
-            while(jobIndex < jobs.length && jobs[jobIndex][0] <= curTime){
+        
+        while(count < n){
+            while(jobIndex < n && jobs[jobIndex][0] <= currentTime){
                 queue.offer(new int[]{jobs[jobIndex][0], jobs[jobIndex][1]});
-                jobIndex += 1;
+                jobIndex++;
+            }
+
+            if(!queue.isEmpty()){
+                int[] job = queue.poll();
+                currentTime += job[1];
+                answer += currentTime - job[0];
+                count += 1;
             }
             
-            if(queue.isEmpty()){
-                if(jobIndex < jobs.length){
-                    curTime = jobs[jobIndex][0];
-                }
-            }
             else{
-                int[] task = queue.poll();
-                curTime += task[1];
-                total += curTime - task[0];
-                count += 1;
+                currentTime = jobs[jobIndex][0];
             }
         }
         
-        return total / count;
+        
+        return answer / n;
     }
 }
