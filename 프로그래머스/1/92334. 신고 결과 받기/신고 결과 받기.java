5e254c63f0625e2ca657857181fd2b59d8e7ed81
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-11-06 15:07:38 +0900

    [level 1] Title: 신고 결과 받기, Time: 183.36 ms, Memory: 164 MB -BaekjoonHub

--- 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
+++ 프로그래머스/1/92334. 신고 결과 받기/신고 결과 받기.java
@@ -2,34 +2,34 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String[] id_list, String[] report, int k) {
-        Map<String, Set<String>> hash = new HashMap<>();
-        Map<String, Integer> answer= new HashMap<>();
-        for(String str: id_list){
-            hash.put(str, new HashSet<>());
-            answer.put(str, 0);
-        }
+        Map<String, Set<String>> map = new HashMap<>();
+        Map<String, Integer> reporterMap = new HashMap<>();
         
-        for(String str: report){
-            String[] strs = str.split(" ");
-            String reporter = strs[0];
-            String reported = strs[1];
-            hash.get(reported).add(reporter);
+        for(String r: report){
+            String[] cur = r.split(" ");
+            map.putIfAbsent(cur[1], new HashSet<>());
+            map.get(cur[1]).add(cur[0]);
         }
-        
-        for(String key: hash.keySet()){
-            if(hash.get(key).size() >= k){
-                for(String reporter: hash.get(key)){
-                    answer.put(reporter, answer.get(reporter) + 1);
+                
+        for(String reported: map.keySet()){
+            if(map.get(reported).size() >= k){
+                for(String reporter: map.get(reported)){
+                    reporterMap.putIfAbsent(reporter, 0);
+                    reporterMap.put(reporter, reporterMap.get(reporter) + 1);
                 }
             }
         }
         
-        int[] result = new int[id_list.length];
+        int[] answer = new int[id_list.length];
+        
         int index = 0;
-        for(String id: id_list){
-            result[index++] = answer.get(id);
+        for(int i = 0; i < id_list.length; i++){
+            String name = id_list[i];
+            if(reporterMap.containsKey(name)){
+                answer[i] = reporterMap.get(name);
+            }
         }
         
-        return result;
+        return answer;
     }
 }
