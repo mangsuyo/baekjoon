5f18343b0b47bf53fbf9f5c2cfc5b12a8f19bf6f
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-11-06 13:42:29 +0900

    [level 1] Title: 신규 아이디 추천, Time: 1.22 ms, Memory: 86.8 MB -BaekjoonHub

--- 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
+++ 프로그래머스/1/72410. 신규 아이디 추천/신규 아이디 추천.java
@@ -1,52 +1,46 @@
+import java.util.*;
+
 class Solution {
     public String solution(String new_id) {
+        String answer = "";
         String first = new_id.toLowerCase();
         StringBuilder second = new StringBuilder();
-        
         for(char c: first.toCharArray()){
-            if(('a' <= c && c <= 'z') || ('0' <= c && c <= '9') || c == '-' || c == '_' || c == '.'){
+            if('a' <= c && c <= 'z' || '0' <= c && c <= '9' || c == '-' || c == '_' || c == '.'){
                 second.append(c);
             }
         }
-        
         StringBuilder third = new StringBuilder();
         boolean isBeforeDot = false;
         for(int i = 0; i < second.length(); i++){
-            char c = second.charAt(i);
-            if(c == '.'){
-                if(!isBeforeDot){
-                    third.append(c);
+            if(second.charAt(i) == '.'){
+                if(isBeforeDot){continue;}
+                else{
                     isBeforeDot = true;
                 }
             }
-            else{
-                third.append(c);
-                isBeforeDot = false;
-            }
+            else{isBeforeDot = false;}
+            third.append(second.charAt(i));
         }
         
-        if(third.length() > 0 && third.charAt(0) == '.') third.deleteCharAt(0);
-        if(third.length() > 0 && third.charAt(third.length() - 1) == '.') third.deleteCharAt(third.length() - 1);
+        
+        if(third.length() > 0 && third.charAt(0) == '.'){
+            third.deleteCharAt(0);
+        }
+        if(third.length() > 0 && third.charAt(third.length() - 1) == '.'){
+            third.deleteCharAt(third.length() - 1);
+        }
         
         if(third.length() == 0) third.append("a");
         
-        StringBuilder sixth = new StringBuilder();
         if(third.length() >= 16){
-            sixth.append(third.toString().substring(0, 15));
-            if(sixth.charAt(sixth.length() - 1) == '.'){
-                sixth.deleteCharAt(sixth.length() - 1);
-            }
-        } else{
-            sixth = new StringBuilder(third);
+            third = new StringBuilder(third.substring(0, 15));
+            if(third.charAt(14) == '.') third.deleteCharAt(14);
         }
         
-        if(sixth.length() > 0){
-            while(sixth.length() <= 2){
-                sixth.append(sixth.charAt(sixth.length() - 1));
-            }
+        while(third.length() < 3){
+            third.append(third.charAt(third.length() - 1));
         }
-        
-        
-        return sixth.toString();
+        return third.toString();
     }
 }
