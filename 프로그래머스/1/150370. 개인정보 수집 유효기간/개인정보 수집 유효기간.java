d5cd0344910da8b853d5a5eb8ae772aba40a1e17
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2025-11-06 15:19:54 +0900

    [level 1] Title: 개인정보 수집 유효기간, Time: 1.48 ms, Memory: 70.6 MB -BaekjoonHub

--- 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
+++ 프로그래머스/1/150370. 개인정보 수집 유효기간/개인정보 수집 유효기간.java
@@ -2,38 +2,43 @@ import java.util.*;
 
 class Solution {
     public int[] solution(String today, String[] terms, String[] privacies) {
-        
-        int now = getDateNumber(today);
-        
+        int todayNumberDate = getNumberDate(today);
         Map<String, Integer> map = new HashMap<>();
+        
         for(int i = 0; i < terms.length; i++){
             String[] cur = terms[i].split(" ");
             map.put(cur[0], Integer.parseInt(cur[1]));
         }
         List<Integer> list = new ArrayList<>();
+        
         for(int i = 0; i < privacies.length; i++){
             String[] cur = privacies[i].split(" ");
-            int extraMonth = map.get(cur[1]);
-            int after = getDateNumber(cur[0]) + 28 * extraMonth;
-            if(now >= after){
-                list.add(i + 1);
+            int nextDate = getNumberDate(cur[0]) + map.get(cur[1]) * 28;
+            if(todayNumberDate >= nextDate){
+                list.add(i);
             }
         }
+        
+        int index = 0;
         int[] answer = new int[list.size()];
-        
-        for(int i = 0; i < list.size(); i++){
-            answer[i] = list.get(i);
+
+        for(Integer num: list){
+            answer[index++] = num + 1;
         }
-        
         return answer;
     }
     
-    
-    int getDateNumber(String date){
+    int getNumberDate(String date) {
         String[] cur = date.split("\\.");
-        int year = Integer.parseInt(cur[0]) - 2000;
-        int month = Integer.parseInt(cur[1]) - 1;
-        int day = Integer.parseInt(cur[2]) - 1;
-        return year * 12 * 28 + month * 28 + day;
+        int year = Integer.parseInt(cur[0]);
+        int month = Integer.parseInt(cur[1]);
+        int day = Integer.parseInt(cur[2]);
+        
+        int numberDate = 0;
+        numberDate += (year - 2000) * 12 * 28;
+        numberDate += (month - 1) * 28;
+        numberDate += day;
+        
+        return numberDate;
     }
 }
