f1299795e142940a96702a2ac2cd122a27885cac
mangsuyo <152208248+mangsuyo@users.noreply.github.com>
2026-03-28 23:08:04 +0900

    [level 3] Title: 미로 탈출 명령어, Time: 2.89 ms, Memory: 78.5 MB -BaekjoonHub

--- 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
+++ 프로그래머스/3/150365. 미로 탈출 명령어/미로 탈출 명령어.java
@@ -1,60 +1,47 @@
 import java.util.*;
 
 class Solution {
-    
-    List<String> answers = new ArrayList<>();
+    char[] d = new char[]{'d', 'l', 'r', 'u'};
+    int[] dr = new int[]{1, 0, 0, -1};
+    int[] dc = new int[]{0, -1, 1, 0};
+    int r;
+    int c;
     int n;
     int m;
+    StringBuilder sb = new StringBuilder();
     
     public String solution(int n, int m, int x, int y, int r, int c, int k) {
-        String answer = "";
-        this.n = n;    
+        x--;
+        y--;
+        r--;
+        c--;
+        this.r = r;
+        this.c = c;
+        this.n = n;
         this.m = m;
         
-        int dist = Math.abs(x - r) + Math.abs(y - c);
-        
-        if(dist % 2 == 0){
-            if(k % 2 != 0) return "impossible";
-        }
-        else{
-            if(k % 2 == 0) return "impossible";
-        }
-                
-        if(Math.abs(x - r) + Math.abs(y - c) > k){
+        if(dfs(x, y, 0, k)){
+            return sb.toString();
+        }else{
             return "impossible";
         }
-        
-        if(!dfs(x - 1, y - 1, r - 1, c - 1, 0, k, "")){
-            return "impossible";
-        };
-
-        
-        return answers.size() == 0 ? "impossible" : answers.get(0);
     }
     
-    boolean dfs(int curR, int curC, int endR, int endC, int depth, int k, String path){
+    
+    boolean dfs(int x, int y, int step, int k){
+        if(x < 0 || x >= n || y < 0 || y >= m) return false;
+        if(x == r && y == c && step == k) return true;
+        int dist = Math.abs(r - x) + Math.abs(c - y);
+        if(dist > k - step) return false;
+        if((k - step - dist) % 2 != 0) return false;
         
-        if(depth > k) return false;
-        
-        if(curR < 0 || curR >= n || curC < 0 || curC >= m) return false;
-        
-        if((Math.abs(curR - endR) + Math.abs(curC - endC)) > k - depth) return false;
-        if(curR == endR && curC == endC && depth == k){
-            answers.add(path);
-            return true;
+        for(int i = 0; i < 4; i++){
+            int nextR = x + dr[i];
+            int nextC = y + dc[i];
+            sb.append(d[i]);
+            if(dfs(nextR, nextC, step + 1, k)) return true;
+            sb.deleteCharAt(sb.length() - 1);
         }
-        
-        if(dfs(curR + 1, curC, endR, endC, depth + 1, k, path + "d")){
-            return true;
-        };
-        if(dfs(curR, curC - 1, endR, endC, depth + 1, k, path + "l")){
-            return true;
-        };        
-        if(dfs(curR, curC + 1, endR, endC, depth + 1, k, path + "r")){
-            return true;
-        };
-        if(dfs(curR - 1, curC, endR, endC, depth + 1, k, path + "u")) return true;
-        
         return false;
     }
 }
